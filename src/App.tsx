ced0ab4c38cc5b896048b66a9ea332ef90433f01
nembleton <nicolas.embleton@gmail.com>
2025-01-18 16:17:30 +0700

    Clean-up CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 // src/App.tsx
 
-import React from 'react';
-import NutritionForm from './components/NutritionForm';
+import React from "react";
+import NutritionForm from "./components/NutritionForm";
 
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Nutrition Calculator</h1>
       <NutritionForm />
     </div>
   );
