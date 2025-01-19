0838841cc052bc52cc8dd2a429442e082f2bf86e
nembleton <nicolas.embleton@gmail.com>
2025-01-19 15:38:10 +0700

    Update nutrition report to use new component

--- src/components/nutrition/NutritionReport.tsx
+++ src/components/nutrition/NutritionReport.tsx
@@ -59,104 +59,46 @@ export const NutritionReport = ({
           },
         ]}
       />
-
-      {/* Carbs Section - Second Column */}
-      <div className="nutrition-section-group">
-        <div className="nutrition-section-title">Carbohydrates</div>
-        <div className="nutrient">
-          <span className="nutrient-name">Total Carbohydrate</span>
-          <span className="nutrient-value">
-            {totalNutrition.totalCarbohydrate.toFixed(2)}g
-          </span>
-        </div>
-        <div className="nutrient">
-          <span className="nutrient-name">Dietary Fiber</span>
-          <span className="nutrient-value">
-            {totalNutrition.dietaryFiber.toFixed(2)}g
-          </span>
-        </div>
-        <div className="nutrient">
-          <span className="nutrient-name">Sugars</span>
-          <span className="nutrient-value">
-            {totalNutrition.sugars.toFixed(2)}g
-          </span>
-        </div>
-      </div>
-
-      {/* Protein Section - Full Width */}
-      <div className="nutrition-section-group full-width">
-        <div className="nutrition-section-title">Protein</div>
-        <div className="nutrient">
-          <span className="nutrient-name">Protein</span>
-          <span className="nutrient-value">
-            {totalNutrition.protein.toFixed(2)}g
-          </span>
-        </div>
-      </div>
-
-      {/* Minerals Section - First Column */}
-      <div className="nutrition-section-group">
-        <div className="nutrition-section-title">Minerals</div>
-        <div className="nutrient">
-          <span className="nutrient-name">Sodium</span>
-          <span className="nutrient-value">
-            {totalNutrition.sodium.toFixed(2)}mg
-          </span>
-        </div>
-        <div className="nutrient">
-          <span className="nutrient-name">Iron</span>
-          <span className="nutrient-value">
-            {totalNutrition.iron.toFixed(2)}mg
-          </span>
-        </div>
-        <div className="nutrient">
-          <span className="nutrient-name">Calcium</span>
-          <span className="nutrient-value">
-            {totalNutrition.calcium.toFixed(2)}mg
-          </span>
-        </div>
-        <div className="nutrient">
-          <span className="nutrient-name">Magnesium</span>
-          <span className="nutrient-value">
-            {totalNutrition.magnesium.toFixed(2)}mg
-          </span>
-        </div>
-        <div className="nutrient">
-          <span className="nutrient-name">Potassium</span>
-          <span className="nutrient-value">
-            {totalNutrition.potassium.toFixed(2)}mg
-          </span>
-        </div>
-        <div className="nutrient">
-          <span className="nutrient-name">Zinc</span>
-          <span className="nutrient-value">
-            {totalNutrition.zinc.toFixed(2)}mg
-          </span>
-        </div>
-      </div>
-
-      {/* Vitamins Section - Second Column */}
-      <div className="nutrition-section-group">
-        <div className="nutrition-section-title">Vitamins</div>
-        <div className="nutrient">
-          <span className="nutrient-name">Vitamin A</span>
-          <span className="nutrient-value">
-            {totalNutrition.vitaminA.toFixed(2)}IU
-          </span>
-        </div>
-        <div className="nutrient">
-          <span className="nutrient-name">Vitamin C</span>
-          <span className="nutrient-value">
-            {totalNutrition.vitaminC.toFixed(2)}mg
-          </span>
-        </div>
-        <div className="nutrient">
-          <span className="nutrient-name">Vitamin D</span>
-          <span className="nutrient-value">
-            {totalNutrition.vitaminD.toFixed(2)}IU
-          </span>
-        </div>
-      </div>
+      <NutrientSection
+        title="Carbohydrates"
+        nutrients={[
+          {
+            name: "Total Carbohydrate",
+            value: totalNutrition.totalCarbohydrate,
+            unit: "g",
+          },
+          {
+            name: "Dietary Fiber",
+            value: totalNutrition.dietaryFiber,
+            unit: "g",
+          },
+          { name: "Sugars", value: totalNutrition.sugars, unit: "g" },
+        ]}
+      />
+      <NutrientSection
+        title="Protein"
+        nutrients={[{ name: "Protein", value: totalNutrition.protein, unit: "g" }]}
+        fullWidth
+      />
+      <NutrientSection
+        title="Minerals"
+        nutrients={[
+          { name: "Sodium", value: totalNutrition.sodium, unit: "mg" },
+          { name: "Iron", value: totalNutrition.iron, unit: "mg" },
+          { name: "Calcium", value: totalNutrition.calcium, unit: "mg" },
+          { name: "Magnesium", value: totalNutrition.magnesium, unit: "mg" },
+          { name: "Potassium", value: totalNutrition.potassium, unit: "mg" },
+          { name: "Zinc", value: totalNutrition.zinc, unit: "mg" },
+        ]}
+      />
+      <NutrientSection
+        title="Vitamins"
+        nutrients={[
+          { name: "Vitamin A", value: totalNutrition.vitaminA, unit: "IU" },
+          { name: "Vitamin C", value: totalNutrition.vitaminC, unit: "mg" },
+          { name: "Vitamin D", value: totalNutrition.vitaminD, unit: "IU" },
+        ]}
+      />
     </div>
   </motion.div>
 );
